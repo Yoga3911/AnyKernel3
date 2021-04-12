8d1f4ac1654dbd6802f2a4957468ffe5db93367f
Robin Hood <74900723+Yoga3911@users.noreply.github.com>
2021-04-12 07:47:16 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,18 +4,14 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Excalibur Kernel | Lavender
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
-supported.versions=
+device.name1=lavender
+supported.versions=9 - 11
 supported.patchlevels=
 '; } # end properties
 
